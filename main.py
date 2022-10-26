c5927aaf2ed2a332cfe891aad2edd2466627bfc0
Atlasdev08 <114253043+Atlasdev08@users.noreply.github.com>
2022-10-26 19:55:27 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,82 @@
+from cProfile import label
+import asyncio
+import discord
+from discord import ButtonStyle
+from discord.ext import commands
+from discord.ui import Button, View
+
+import time
+
+
+from vinted import *
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot =commands.Bot(command_prefix='!',description="bot teste",intents=intents)
+"""@bot.event
+async def on_ready():
+    print("ready")"""
+
+
+posting = []
+@bot.command()
+
+
+
+async def bouton(ctx):
+    while True:
+        try:
+            z = search("https://www.vinted.fr/vetements?catalog%5B%5D=76&order=newest_first&price_to=15&currency=EUR&brand_id%5B%5D=304")
+            x = z["items"]["catalogItems"]["byId"]
+            dictlist = list(x)
+            for i in range(9, -1, -1):
+                    time.sleep(0)
+                    post = dictlist[i - 1]
+                    if str(post) in posting:
+                        print(f"{Spy.blanc}[{Spy.rouge}{post}{Spy.blanc}] - Item déjà envoyé !")
+                        time.sleep(0)
+                        continue
+                    else:
+                        print(f"{Spy.blanc}[{Spy.vert}{post}{Spy.blanc}] - Nouvel item trouvé !")
+                        info = get_info_post(x[str(post)]["url"])
+                        posting.append(str(post))
+
+
+                        embed=discord.Embed(title=f"{x[post]['title']}",url=f"{x[post]['url']}",color=0xef9aff)
+                        embed.set_author(name="RESELL EXPO",icon_url="https://cdn.discordapp.com/avatars/1010967491733901482/b068cfb2fa571f847b0b959060702dc6.png")
+                        embed.set_image(url=f"{x[post]['photo']['thumbnails'][4]['url']}")
+                        embed.add_field(name="Auteur",value=f"{info['username']}",inline=False)
+                        embed.add_field(name="Prix",value=f"{x[post]['price']} €")
+                        embed.add_field(name="Taille",value=f"{x[post]['size_title']}")
+                        embed.add_field(name="Marque",value=f"{x[post]['brand_title']}")
+                        buttonacheter = Button(label="Acheter",style=discord.ButtonStyle.green, emoji="💵",url=f"https://www.vinted.fr/transaction/buy/new?source_screen=item&transaction%5Bitem_id%5D={str(post)}")
+                        buttonfavorie = Button(label="Négociation",style=discord.ButtonStyle.danger, emoji="🤝",url=f"https://www.vinted.fr/items/{str(post)}/want_it/new?button_name=receiver_id={str(post)}")
+                        buttondetails = Button(label="Détails",style=discord.ButtonStyle.grey, emoji="🔎",url=f"{x[post]['url']}")
+                        
+                        
+                        view=View()
+                        view.add_item(buttondetails)
+                        view.add_item(buttonacheter)
+                        view.add_item(buttonfavorie)
+                                        
+
+                       
+                        print(f"{Spy.blanc}[{Spy.bleu}ENVOYER{Spy.blanc}] - L'article a été envoyer ! {Spy.vert}{post}")
+                        await ctx.send(embed=embed,view=view)
+                        time.sleep(0)
+        except:
+            print(f"{Spy.blanc}[{Spy.rouge}ERREUR{Spy.blanc}] - L'envoi na pas été réusi!")
+            time.sleep(0)
+
+
+
+
+
+
+
+
+bot.run('MTAzMjM0ODc4NDE1NzU0MDM5Mg.GMBE-v.ftkALs-vOa7hRWsJmXV_CQMYriufH-bwGKsIXc')
+
+#MTAzMjM1NzIwNDM5NDc3MDQ3NA.GIQsl9.S7sIrv1c_s1N3kMPwjCj4YaDDkxExN5xh-FUww   mon serveur
+
+#MTAzMjM0ODc4NDE1NzU0MDM5Mg.GMBE-v.ftkALs-vOa7hRWsJmXV_CQMYriufH-bwGKsIXc
---
+++ vinted.py
@@ -0,0 +1,178 @@
+# Author : 2$py#5430
+# Discord : discord.gg/spyy
+# Github : https://github.com/2spy/
+# Updated at : 04/07/2022 12:55
+# Version : 4.3
+# Description : Vinted Bot
+# Language : Python
+import discord
+from discord import ButtonStyle
+from discord.ext import commands
+from discord.ui import Button, View
+ 
+
+import os
+import json
+import shutil
+import threading
+import time
+import sys
+try:
+    import requests
+    from bs4 import BeautifulSoup
+except:
+    os.system("pip install requests")
+    os.system("pip install bs4")
+
+
+from cProfile import label
+import asyncio
+import discord
+from discord import ButtonStyle
+from discord.ext import commands
+from discord.ui import Button, View
+
+
+
+class Spy:
+    gris = "\033[1;30;1m"
+    rouge = "\033[1;31;1m"
+    vert = "\033[1;32;1m"
+    rose=""
+    jaune = "\033[1;33;1m"
+    bleu = "\033[1;34;1m"
+    violet = "\033[1;35;1m"
+    cyan = "\033[1;36;1m"
+    blanc = "\033[1;0;1m"
+
+
+
+def get_info_post(url):
+    try:
+        time.sleep(1)
+        print(f"{Spy.blanc}[{Spy.jaune}RECHERCHE{Spy.blanc}] - Le bot recupere les informations de l'item...")
+        headers = requests.utils.default_headers()
+        headers.update({'User-agent': 'Mozilla/5.0'})
+        reponse = requests.get(str(url), headers=headers)
+        if 429 == reponse.status_code:
+            print(f"{Spy.blanc}[{Spy.rouge}ERREUR{Spy.blanc}] - Rate Limit !")
+            time.sleep(1)
+        soup = BeautifulSoup(reponse.text, "html.parser")
+
+        res = soup.findAll('script', {"class": "js-react-on-rails-component"})
+
+        descindice = 0
+        userinfoindice = 0
+        for i in range(len(res)):
+            if 'data-component-name="ItemDescription"' in str(res[i]).split():
+                descindice = i
+            if 'data-component-name="ItemUserInfo"' in str(res[i]).split():
+                userinfoindice = i
+
+        description = json.loads(res[descindice].text.replace(
+            '<script class="js-react-on-rails-component" data-component-name="ItemDescription" data-dom-id="ItemDescription-react-component-3d79657d-a1b5-4f1d-b501-2f470f328c66" type="application/json">',
+            "").replace("</script>", ''))
+
+        userinfo = json.loads(res[userinfoindice].text.replace(
+            '<script class="js-react-on-rails-component" data-component-name="ItemUserInfo" data-dom-id="ItemUserInfo-react-component-2105d904-b161-47d1-bfce-9b897a8c1cc6" type="application/json">',
+            '').replace("</script>", ''))
+
+        titre = description["content"]["title"]
+        description = description["content"]["description"]
+        positive = userinfo["user"]["positive_feedback_count"]
+        negative = userinfo["user"]["negative_feedback_count"]
+        username = userinfo["user"]["login"]
+        pays = userinfo["user"]["country_title"]
+        ville = userinfo["user"]["city"]
+
+        lesinfo = {}
+
+        if titre == "":
+            titre = "Pas de donnée"
+        if description == "":
+            description = "Pas de donnée"
+        if positive == "":
+            positive = "Pas de donnée"
+        if negative == "":
+            negative = "Pas de donnée"
+        if username == "":
+            username = "Pas de donnée"
+        if pays == "":
+            pays = "Pas de donnée"
+        if ville == "":
+            ville = "Pas de donnée"
+
+        try:
+            lesinfo["titre"] = titre
+            lesinfo["description"] = description
+            lesinfo["positive"] = positive
+            lesinfo["negative"] = negative
+            lesinfo["username"] = username
+            lesinfo["pays"] = pays
+            lesinfo["ville"] = ville
+        except Exception as err:
+            print(err)
+        return lesinfo
+    except:
+        pass
+
+
+def search(url):
+    try:
+        time.sleep(1)
+        print(f"{Spy.blanc}[{Spy.gris}RECHERCHE{Spy.blanc}] - Le bot cherche des nouveaux items...")
+        headers = requests.utils.default_headers()
+        headers.update({'User-agent': 'Mozilla/5.0'})
+        reponse = requests.get(str(url), headers=headers)
+        print(reponse)
+        if 429 == reponse.status_code:
+            print(f"{Spy.blanc}[{Spy.rouge}ERREUR{Spy.blanc}] - Rate Limit !")
+            time.sleep(1)
+        soup = BeautifulSoup(reponse.text, "html.parser")
+
+        res = soup.findAll('script')
+        indices = 0
+        for i in range(len(res) + 1):
+            if 'data-js-react-on-rails-store="MainStore"' in str(res[i]).split():
+                indices += i
+                break
+        value = res[indices].text.replace('<script z-js-react-on-rails-store="MainStore" type="application/json">', "")
+        z = json.loads(value)
+        with open('data.json', 'w') as mon_fichier:
+            json.dump(z, mon_fichier)
+
+        return z
+    except:
+        print(f"{Spy.blanc}[{Spy.rouge}ERREUR{Spy.blanc}] - La fonction search na pas fonctionner!")
+        pass
+"""  
+posting = []
+
+def teste():
+    while True:
+        z = search("https://www.vinted.fr/vetements?catalog%5B%5D=76&brand_id%5B%5D=53&order=newest_first&price_to=12&currency=EUR")
+        x = z["items"]["catalogItems"]["byId"]
+        dictlist = list(x)
+        for i in range(9, -1, -1):
+                time.sleep(1)
+                post = dictlist[i - 1]
+                if str(post) in posting:
+                    print(f"{Spy.blanc}[{Spy.rouge}{post}{Spy.blanc}] - Item déjà envoyé !")
+                    time.sleep(1)
+                    continue
+                else:
+                    print(f"{Spy.blanc}[{Spy.vert}{post}{Spy.blanc}] - Nouvel item trouvé !")
+                    info = get_info_post(x[str(post)]["url"])
+                    posting.append(str(post))
+                    print(posting)
+                    intents = discord.Intents.default()
+                    intents.message_content = True
+                    bot =commands.Bot(command_prefix='!',description="bot teste",intents=intents)
+                    print("le bot est en ligne ")
+                    bot.run('MTAzMjM1NzIwNDM5NDc3MDQ3NA.GoNQMF.1hGvCooNn0aF-TyJJhzADNgrTlbkVTGJSPiq5U')
+
+
+
+    icon_url=ctx.author.avatar.url 
+teste()
+"""  
